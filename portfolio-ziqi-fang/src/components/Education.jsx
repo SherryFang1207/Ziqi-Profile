31454e3c8d74d1decc81d055756d9c1b50253dae
Ziqi Fang <zfang1207@gmail.com>
2023-09-22 17:10:12 -0700

    add Sushier and update portfolio photo

--- portfolio-ziqi-fang/src/components/Education.jsx
+++ portfolio-ziqi-fang/src/components/Education.jsx
@@ -1,63 +1,122 @@
-import React from 'react'
-import Section from './common/Section'
-import UCSC_Icon from '../assets/UCSC.png'
-import AWS_badge from '../assets/aws-certified-cloud-practitioner.png'
-import {FaArrowDown} from 'react-icons/fa'
-
-
-
+import React from "react";
+import Section from "./common/Section";
+import UCSC_Icon from "../assets/UCSC.png";
+import AWS_Practitioner_Badge from "../assets/aws-certified-cloud-practitioner.png";
+import AWS_SAA_Badge from "../assets/aws-certified-solutions-architect-associate.png";
+import { FaArrowDown } from "react-icons/fa";
 
 const Education = () => {
   return (
-    <Section title='Education and Certificates' subtitle='“Education is not preparation for life; education is life itself.” —John Dewey'>
-        <section className='w-5/6 flex flex-col gap-y-20 xl:flex-row xl:gap-x-20 justify-between items-center '>
-            <div className='w-1/2 flex flex-col xl:flex-row gap-2 justify-evenly items-center'>
-                {/* picture */}
-                <div className='flex shrink-0'>
-                    <img src={UCSC_Icon} alt='UCSC Icon' className='flex w-60 h-auto '></img>
-                </div>
-                {/* Description */}
-                <div className='flex flex-col justify-center items-center'>
-                    <h2 className='tracking-wide font-semibold text-2xl py-2'> UCSC Computer Science B.S</h2>
-                    <p>The Computer Science and Engineering (CSE) department spans multiple areas of research including theory, systems, AI/ML, architectures, and software.</p>
-                    {/* before:from-[#57DDF3] before:to-[#C7F954] */}
-                    <p className='flex text-center text-lg font-medium text-[#43c79f]'>Aquire Time: 2019-2022</p>
-                    <p className='flex text-center text-md font-normal text-[#27725b]'>Issued By: University of California, Santa Cruz</p>
-                    
-                </div>
-            </div>
+    <Section
+      title="Education and Certificates"
+      subtitle="“Education is not preparation for life; education is life itself.” —John Dewey"
+    >
+      <section className="w-5/6 flex flex-col gap-y-20 xl:flex-row xl:gap-x-20 justify-between items-center ">
+        <div className="w-1/3 flex flex-col xl:flex-row gap-2 justify-evenly items-center">
+          {/* picture */}
+          <div className="flex shrink-0">
+            <img
+              src={UCSC_Icon}
+              alt="UCSC Icon"
+              className="flex w-60 h-auto "
+            ></img>
+          </div>
+          {/* Description */}
+          <div className="flex flex-col justify-center items-center">
+            <h2 className="tracking-wide font-semibold text-2xl py-2">
+              {" "}
+              UCSC Computer Science B.S
+            </h2>
+            <p>
+              The Computer Science and Engineering (CSE) department spans
+              multiple areas of research including theory, systems, AI/ML,
+              architectures, and software.
+            </p>
+            {/* before:from-[#57DDF3] before:to-[#C7F954] */}
+            <p className="flex text-center text-lg font-medium text-[#43c79f]">
+              Aquire Time: 2019-2023
+            </p>
+            <p className="flex text-center text-md font-normal text-[#27725b]">
+              Issued By: University of California, Santa Cruz
+            </p>
+          </div>
+        </div>
 
-            {/* 
+        {/* 
             Badge link: https://www.credly.com/badges/f0b84dc2-6a9c-46d4-8c89-2697a9be2de9/public_url
              */}
-             {/* Parameters:
+        {/* Parameters:
              1. certification name
              2. certification image
              3. certification link
              4. certification description
              5. issuer
               */}
-            <div className='w-1/2 flex flex-col xl:flex-row gap-2 justify-evenly items-center'>
-                {/* picture */}
-                <div className='flex shrink-0'>
-                    <img src={AWS_badge} alt='aws_badge' className='flex w-60 h-auto '></img>
-                </div>
-                {/* Description */}
-                <div className='flex flex-col justify-center items-center'>
-                    <h2 className='tracking-wide font-semibold text-2xl py-2'> AWS Certified Cloud Practitioner</h2>
-                    <p>The AWS Certified Cloud Practitioner validates foundational, high-level understanding of AWS Cloud, services, and terminology.</p>
-                    {/* before:from-[#57DDF3] before:to-[#C7F954] */}
-                    <p className='flex text-center text-lg font-medium text-[#43c79f]'>Aquire Time: 2023</p>
-                    <p className='flex text-center text-md font-normal text-[#27725b]'>Issued By: Amazon Web Services Training and Certification</p>
-                </div>
-            </div>
+        <div className="w-1/3 flex flex-col xl:flex-row gap-2 justify-evenly items-center">
+          {/* picture */}
+          <div className="flex shrink-0">
+            <img
+              src={AWS_Practitioner_Badge}
+              alt="AWS_Practitioner_Badge"
+              className="flex w-60 h-auto "
+            ></img>
+          </div>
+          {/* Description */}
+          <div className="flex flex-col justify-center items-center">
+            <h2 className="tracking-wide font-semibold text-2xl py-2">
+              {" "}
+              AWS Certified Cloud Practitioner
+            </h2>
+            <p>
+              The AWS Certified Cloud Practitioner validates foundational,
+              high-level understanding of AWS Cloud, services, and terminology.
+            </p>
+            {/* before:from-[#57DDF3] before:to-[#C7F954] */}
+            <p className="flex text-center text-lg font-medium text-[#43c79f]">
+              Aquire Time: 2023
+            </p>
+            <p className="flex text-center text-md font-normal text-[#27725b]">
+              Issued By: Amazon Web Services Training and Certification
+            </p>
+          </div>
+        </div>
+        {/* aws-certified-solutions-architect-associate */}
+        <div className="w-1/3 flex flex-col xl:flex-row gap-2 justify-evenly items-center">
+          {/* picture */}
+          <div className="flex shrink-0">
+            <img
+              src={AWS_SAA_Badge}
+              alt="AWS_Practitioner_Badge"
+              className="flex w-60 h-auto "
+            ></img>
+          </div>
+          {/* Description */}
+          <div className="flex flex-col justify-center items-center">
+            <h2 className="tracking-wide font-semibold text-2xl py-2">
+              {" "}
+              AWS Certified Solutions Architect - Associate
+            </h2>
+            <p>
+              AWS Certified Solutions Architect - Associate: Expert in
+              cost-effective, high-performance AWS solutions, aligned with the
+              Well-Architected Framework.
+            </p>
+            {/* before:from-[#57DDF3] before:to-[#C7F954] */}
+            <p className="flex text-center text-lg font-medium text-[#43c79f]">
+              Aquire Time: 2023
+            </p>
+            <p className="flex text-center text-md font-normal text-[#27725b]">
+              Issued By: Amazon Web Services Training and Certification
+            </p>
+          </div>
+        </div>
+      </section>
 
-        </section>
-
-        <p className='pt-20 text-center tracking-wide text-xl drop-shadow-sm'>... And More Certificates in Progress! 💫</p>
-        
+      <p className="pt-20 text-center tracking-wide text-xl drop-shadow-sm">
+        ... And More Certificates in Progress! 💫
+      </p>
     </Section>
-  )
-}
+  );
+};
 
-export default Education
+export default Education;
--- portfolio-ziqi-fang/src/components/Portfolio.jsx
+++ portfolio-ziqi-fang/src/components/Portfolio.jsx
@@ -1,78 +1,168 @@
-import React from 'react'
-import Section from './common/Section'
+import React from "react";
+import Section from "./common/Section";
 
-import {FaGithub, FaExternalLinkAlt} from 'react-icons/fa'
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
-
-import Promptopia_thumbnail from '../assets/thumbnails/Promptopia-thumbnail.png'
-import Clipboard_thumbnail from '../assets/thumbnails/clipboard-thumbnail.png'
-import Restaurant_travel_advisor from '../assets/thumbnails/Restaurant-travel-advisor-thumbnail.png'
-import Movieland_thumbnail from '../assets/thumbnails/MovieLand-thumbnail.png'
+import Promptopia_thumbnail from "../assets/thumbnails/Promptopia-thumbnail.png";
+import Clipboard_thumbnail from "../assets/thumbnails/clipboard-thumbnail.png";
+import Restaurant_travel_advisor from "../assets/thumbnails/Restaurant-travel-advisor-thumbnail.png";
+import Movieland_thumbnail from "../assets/thumbnails/MovieLand-thumbnail.png";
+import Sushier_Snapshot from "../assets/sushier-app-snapshot.png";
 
 export const Portfolio = () => {
-    const projects = [
-        {
-            id: 1,
-            image: Promptopia_thumbnail,
-            title: "Promptopia",
-            description:"Create, edit, share and view Great AI prompts on Promptopia! 🙌🏻",
-            github: "https://github.com/SherryFang1207/Promptopia-app",
-            demo: "https://promptopia-app-psi.vercel.app/",
-        },
-        {
-            id: 2,
-            image: Clipboard_thumbnail,
-            title: "Clip Board App",
-            description:"A decent frontend page for a great future Clipboard App!🖊️",
-            github: "https://github.com/SherryFang1207/clipboard-project",
-            demo: "https://ziqifangclipboardapp.netlify.app/#",
-        },
-        {
-            id: 3,
-            image: Restaurant_travel_advisor,
-            title:"Restaurant Travel Advisor",
-            description: "Don't know what to eat during travel? Consult our Restaurant Travel Advisor! 🍔",
-            github: "https://github.com/SherryFang1207/Travel-Restaurant-Advisor-App",
-            demo: "https://travel-restaurant-advisor-ziqi-fang.netlify.app/",
-        },
-        {
-            id: 4,
-            image: Movieland_thumbnail,
-            title: "MovieLand",
-            description:"Search and find your favorite movies! 🎥",
-            github: "https://github.com/SherryFang1207/MovieLand-App",
-            demo: "https://movieland-ziqi-fang.netlify.app",
-        },
-        
-    ];
+  const projects = [
+    {
+      id: 1,
+      image: Promptopia_thumbnail,
+      title: "Promptopia",
+      description:
+        "Create, edit, share and view Great AI prompts on Promptopia! 🙌🏻",
+      github: "https://github.com/SherryFang1207/Promptopia-app",
+      demo: "https://promptopia-app-psi.vercel.app/",
+    },
+    {
+      id: 2,
+      image: Clipboard_thumbnail,
+      title: "Clip Board App",
+      description: "A decent frontend page for a great future Clipboard App!🖊️",
+      github: "https://github.com/SherryFang1207/clipboard-project",
+      demo: "https://ziqifangclipboardapp.netlify.app/#",
+    },
+    {
+      id: 3,
+      image: Restaurant_travel_advisor,
+      title: "Restaurant Travel Advisor",
+      description:
+        "Don't know what to eat during travel? Consult our Restaurant Travel Advisor! 🍔",
+      github: "https://github.com/SherryFang1207/Travel-Restaurant-Advisor-App",
+      demo: "https://travel-restaurant-advisor-ziqi-fang.netlify.app/",
+    },
+    {
+      id: 4,
+      image: Movieland_thumbnail,
+      title: "MovieLand",
+      description: "Search and find your favorite movies! 🎥",
+      github: "https://github.com/SherryFang1207/MovieLand-App",
+      demo: "https://movieland-ziqi-fang.netlify.app",
+    },
+  ];
+  const lastest_project = [
+    {
+      id: 1,
+      image: Sushier_Snapshot,
+      title: "Sushier",
+      description:
+        "Experience Sushier – where sushi meets cutting-edge technology! Explore Sushier online E-commerce platform, meticulously crafted using MERN stack(React, Next.js, Express.js and MongoDB Atlas) and modern web development to bring you a seamless sushi ordering experience.",
+      github: "https://github.com/SherryFang1207/sushier-app",
+      demo: "https://www.sushier.app/",
+    },
+  ];
 
   return (
-    <Section title="Portfolio📃"
-    subtitle="Some of my recent projects are displayed here. I hope you like them!"
+    <Section
+      title="Portfolio📃"
+      subtitle="Some of my recent projects are displayed here. I hope you like them!"
     >
-    <div className='grid gap-8 lg:gap-14 xl:grid-cols-2'>
-        {projects.map(({id, image, title,description, github, demo}) => (
-            <div key={id}
-                className='max-w-3xl flex flex-col md:flex-row shadow-lg shadow-gray-300 rounded-2xl overflow-hidden my-4'
+      <div className="mb-10 -mt-5 hidden xl:block">
+        {lastest_project.map(
+          ({ id, image, title, description, github, demo }) => (
+            <div
+              key={id}
+              className="max-w-5xl flex flex-col md:flex-row shadow-lg shadow-gray-300 rounded-2xl overflow-hidden my-4"
             >
-                <img src={image} alt={title} className='w-full md:w-2/3 my-2'/>
-                <div className='w-full md:w-1/3 flex flex-col items-center justify-evenly p-2'>
-                    <h2 className='tracking-wide text-xl font-semibold'>{title}</h2>
-                    <p>{description}</p>
-                    <div className='flex flex-row w-full justify-evenly my-4'>
-                    <a href={github} className='text-lg md:text-xl lg:text-2xl cursor-pointer duration-150 hover:scale-110' target='_blank' rel='noopener noreferrer'>
-                        <FaGithub/>
-                    </a>
-                    <a href={demo} className='text-lg md:text-xl lg:text-2xl cursor-pointer duration-150 hover:scale-110' target='_blank' rel='noopener noreferrer'>
-                        <FaExternalLinkAlt/>
-                    </a>
-                    </div>
-                    
+              <img src={image} alt={title} className="w-full md:w-2/3 my-2" />
+              <div className="w-full md:w-1/3 flex flex-col items-center justify-evenly p-2">
+                <h2 className="tracking-wide text-xl font-semibold">{title}</h2>
+                <p>{description}</p>
+                <div className="flex flex-row w-full justify-evenly my-4">
+                  <a
+                    href={github}
+                    className="text-lg md:text-xl lg:text-2xl cursor-pointer duration-150 hover:scale-110"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaGithub size={30} />
+                  </a>
+                  <a
+                    href={demo}
+                    className="text-lg md:text-xl lg:text-2xl cursor-pointer duration-150 hover:scale-110"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaExternalLinkAlt size={30} />
+                  </a>
                 </div>
+              </div>
             </div>
+          )
+        )}
+      </div>
+      <div className="mb-10 -mt-5 block xl:hidden">
+        {lastest_project.map(
+          ({ id, image, title, description, github, demo }) => (
+            <div
+              key={id}
+              className="max-w-3xl flex flex-col md:flex-row shadow-lg shadow-gray-300 rounded-2xl overflow-hidden my-4"
+            >
+              <img src={image} alt={title} className="w-full md:w-2/3 my-2" />
+              <div className="w-full md:w-1/3 flex flex-col items-center justify-evenly p-2">
+                <h2 className="tracking-wide text-xl font-semibold">{title}</h2>
+                <p>{description}</p>
+                <div className="flex flex-row w-full justify-evenly my-4">
+                  <a
+                    href={github}
+                    className="text-lg md:text-xl lg:text-2xl cursor-pointer duration-150 hover:scale-110"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaGithub />
+                  </a>
+                  <a
+                    href={demo}
+                    className="text-lg md:text-xl lg:text-2xl cursor-pointer duration-150 hover:scale-110"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaExternalLinkAlt />
+                  </a>
+                </div>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+      <div className="grid gap-8 lg:gap-14 xl:grid-cols-2">
+        {projects.map(({ id, image, title, description, github, demo }) => (
+          <div
+            key={id}
+            className="max-w-3xl flex flex-col md:flex-row shadow-lg shadow-gray-300 rounded-2xl overflow-hidden my-4"
+          >
+            <img src={image} alt={title} className="w-full md:w-2/3 my-2" />
+            <div className="w-full md:w-1/3 flex flex-col items-center justify-evenly p-2">
+              <h2 className="tracking-wide text-xl font-semibold">{title}</h2>
+              <p>{description}</p>
+              <div className="flex flex-row w-full justify-evenly my-4">
+                <a
+                  href={github}
+                  className="text-lg md:text-xl lg:text-2xl cursor-pointer duration-150 hover:scale-110"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub />
+                </a>
+                <a
+                  href={demo}
+                  className="text-lg md:text-xl lg:text-2xl cursor-pointer duration-150 hover:scale-110"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaExternalLinkAlt />
+                </a>
+              </div>
+            </div>
+          </div>
         ))}
-    </div>
-
+      </div>
     </Section>
-  )
-}
+  );
+};
